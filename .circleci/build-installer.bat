fa314ab30c4aa8e800ea2ba1ba364eefad8cf153
Alan Rynne <alan@speckle.systems>
2024-04-01 23:17:51 +0200

    double %% in bat file

--- .circleci/build-installer.bat
+++ .circleci/build-installer.bat
@@ -1,3 +1,3 @@
-for /f "tokens=1 delims=-" %i in ("%CIRCLE_TAG%") do set "TAG=%i.%WORKFLOW_NUM%"
+for /f "tokens=1 delims=-" %%i in ("%CIRCLE_TAG%") do set "TAG=%%i.%WORKFLOW_NUM%"
 for /f "tokens=1 delims=/" %%j in ("%CIRCLE_TAG%") do set "SEMVER=%%j"
 tools\InnoSetup\ISCC.exe tools\powerbi.iss /Sbyparam=$p /DINFO_VERSION=%TAG% /DVERSION=%SEMVER%
