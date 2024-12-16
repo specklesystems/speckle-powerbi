cb08977efa6fa293858cf6a388a971a9d3989e2c
oguzhankoral <oguzhankoral@gmail.com>
2024-12-16 18:03:10 +0300

    extract out button classes

--- src/powerbi-visual/src/views/HomeView.vue
+++ src/powerbi-visual/src/views/HomeView.vue
@@ -7,21 +7,24 @@
     </div>
     <p class="heading">Speckle PowerBI 3D Visual</p>
     <div class="flex justify-center mt-2 gap-1">
-      <button class="btn p-2 rounded-md bg-transparent border-transparent text-foreground font-medium hover:bg-primary-muted disabled:hover:bg-transparent focus-visible:border-foundation" @click="goToForum">Help</button>
-      <button class="btn p-2 rounded-md bg-transparent border-transparent text-foreground font-medium hover:bg-primary-muted disabled:hover:bg-transparent focus-visible:border-foundation" @click="goToGuide">Getting started</button>
-      <!-- TODO: dependency issue need to be resolved to be able to use ui-components library
-      <FormButton color="invert" @click="goToForum">Help</FormButton>
-      <FormButton color="invert" @click="goToGuide">Getting started</FormButton>
-      -->
+      <button :class="buttonClass" @click="goToForum">Help</button>
+      <button :class="buttonClass" @click="goToGuide">Getting started</button>
+      <!-- TODO: dependency issue need to be resolved to be able to use ui-components library-->
+      <!-- <FormButton color="subtle" @click="goToForum">Help</FormButton>
+      <FormButton color="subtle" @click="goToGuide">Getting started</FormButton> -->
     </div>
+    <!-- <CommonLoadingBar :loading="true"/> -->
   </div>
 </template>
 
 <script setup lang="ts">
 // import { FormButton } from '@speckle/ui-components'
+// import { CommonLoadingBar } from '@speckle/ui-components';
 import { useVisualStore } from '../store/visualStore'
 
 const visualStore = useVisualStore()
+
+const buttonClass = `btn p-2 rounded-md bg-transparent border-transparent text-white font-medium hover:bg-primary-muted hover:text-foreground disabled:hover:bg-transparent focus-visible:border-foundation`
 
 function goToForum() {
   visualStore.host.launchUrl('https://speckle.community/tag/powerbi')
