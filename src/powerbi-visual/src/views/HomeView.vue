fd77bd266b4fcdcc41026e905cd730f9259b4b5e
oguzhankoral <oguzhankoral@gmail.com>
2025-02-17 18:27:24 +0300

    home view

--- src/powerbi-visual/src/views/HomeView.vue
+++ src/powerbi-visual/src/views/HomeView.vue
@@ -1,31 +1,65 @@
 <template>
   <div
     id="speckle-home-view"
-    class="flex flex-col justify-center items-center h-full w-full bg-primary text-center text-foundation"
+    class="flex flex-col justify-center items-center h-full w-full text-center space-y-4 p-2"
   >
-    <div class="flex justify-center items-center">
-      <img src="@assets/logo-white.png" alt="Logo" class="w-1/3" />
+    <div class="flex flex-col justify-center items-center h-full w-full text-center space-y-4">
+      <div class="flex flex-row justify-center items-center space-x-1">
+        <div class="flex justify-center items-center">
+          <img src="@assets/logo-big.png" alt="Logo" class="w-16" />
+        </div>
+        <div
+          class="bg-gradient-to-r from-blue-500 via-blue-400 to-blue-600 inline-block py-1 text-transparent bg-clip-text text-2xl"
+        >
+          <p>
+            <b>Speckle</b>
+            for
+            <b>PowerBI</b>
+          </p>
+        </div>
+      </div>
+      <div class="flex flex-col justify-center space-y-1">
+        <div class="flex flex-row space-x-2">
+          <EyeIcon class="w-6"></EyeIcon>
+          <p><b>Version Object ID</b></p>
+          <ArrowRightIcon class="w-4"></ArrowRightIcon>
+          <p>View your model</p>
+        </div>
+        <div class="flex flex-row space-x-2">
+          <CursorArrowRippleIcon class="w-6"></CursorArrowRippleIcon>
+          <p><b>Object IDs</b></p>
+          <ArrowRightIcon class="w-4"></ArrowRightIcon>
+          <p>Highlighting and interactivity</p>
+        </div>
+        <div class="flex flex-row space-x-2">
+          <ChatBubbleLeftIcon class="w-6"></ChatBubbleLeftIcon>
+          <p><b>Tooltip Data</b></p>
+          <ArrowRightIcon class="w-4"></ArrowRightIcon>
+          <p>Tooltip and interactivity</p>
+        </div>
+      </div>
     </div>
-    <p class="heading">Speckle PowerBI 3D Visual</p>
-    <div class="flex justify-center mt-2 gap-1">
-      <button :class="buttonClass" @click="goToForum">Help</button>
+    <div class="flex justify-end gap-1">
       <button :class="buttonClass" @click="goToGuide">Getting started</button>
-      <!-- TODO: dependency issue need to be resolved to be able to use ui-components library-->
-      <!-- <FormButton color="subtle" @click="goToForum">Help</FormButton>
-      <FormButton color="subtle" @click="goToGuide">Getting started</FormButton> -->
+      <button :class="buttonClass" @click="goToForum">Help</button>
     </div>
-    <!-- <CommonLoadingBar :loading="true"/> -->
   </div>
 </template>
 
 <script setup lang="ts">
-// import { FormButton } from '@speckle/ui-components'
-// import { CommonLoadingBar } from '@speckle/ui-components';
 import { useVisualStore } from '../store/visualStore'
+import {
+  EyeIcon,
+  ArrowRightIcon,
+  CursorArrowRippleIcon,
+  ChatBubbleLeftIcon
+} from '@heroicons/vue/24/outline'
 
 const visualStore = useVisualStore()
 
-const buttonClass = `btn p-2 rounded-md bg-transparent border-transparent text-white font-medium hover:bg-primary-muted hover:text-foreground disabled:hover:bg-transparent focus-visible:border-foundation`
+const buttonClass = `btn p-2 rounded-md bg-primary-muted border-transparent font-medium 
+  hover:bg-gray-200 hover:shadow-md
+  disabled:hover:bg-transparent focus-visible:border-foundation transition-all duration-150`
 
 function goToForum() {
   visualStore.host.launchUrl('https://speckle.community/tag/powerbi')
