d94e4958e67fe146828b692d99e5fdbbf7ee2faf
Mucahit Bilal GOKER <mucahitbgoker@gmail.com>
2025-05-01 13:38:38 +0300

    remove lighting and camera settings from visual settings

--- src/powerbi-visual/src/settings/visualSettingsModel.ts
+++ src/powerbi-visual/src/settings/visualSettingsModel.ts
@@ -1,17 +1,10 @@
 import { formattingSettings as fs } from 'powerbi-visuals-utils-formattingmodel'
 import { ColorSelectorSettings, ColorSettings } from 'src/settings/colorSettings'
-import { CameraSettings } from 'src/settings/cameraSettings'
-import { LightingSettings } from 'src/settings/lightingSettings'
-
 export class SpeckleVisualSettingsModel extends fs.Model {
   // Building my visual formatting settings card
   public color: ColorSettings = new ColorSettings()
 
   public colorSelector: ColorSelectorSettings = new ColorSelectorSettings()
 
-  public camera: CameraSettings = new CameraSettings()
-
-  public lighting: LightingSettings = new LightingSettings()
-
-  cards = [this.color, this.camera, this.lighting]
+  cards = [this.color]
 }
